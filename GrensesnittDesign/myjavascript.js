af4b2815ab10471c79b9e6851f8acb8b1001e065
Aleksander Hougen <aleksander-hougen@hotmail.com>
2013-12-08 23:53:09 +0100

    DELIVERY

---
+++ GrensesnittDesign/myjavascript.js
@@ -0,0 +1,117 @@
+window.onload = function(){
+	console.log('TEST');
+	var retrievedData = handlers.getSums();
+	if ([null, undefined].indexOf(retrievedData) === -1) {
+
+		//alert(retrievedData);
+	} else {
+		alert("NO DATA");
+
+	}
+};
+
+function addElement(){
+
+	var linkName = prompt("Enter name of new item", "");
+	
+	if(linkName != null && linkName != ""){
+		var denotation = ",-";
+
+		var buttonString = "Button";
+
+		var currentList = document.getElementById("itemlist");
+		
+
+		var newLi = document.createElement("li");
+		var newName = document.createElement("span");
+		var newSum = document.createElement("span");
+		
+		var newForm = document.createElement("input");
+		var newButton = document.createElement("button");
+
+		var buttonText = document.createTextNode("+");
+
+		newButton.appendChild(buttonText);
+
+		var linkText = document.createTextNode(linkName);
+		newName.appendChild(linkText);
+		newLi.appendChild(newName);
+
+		var textNode = document.createTextNode("0,-");
+
+		newSum.appendChild(textNode);
+
+		newName.className = "itemvalue";
+		newSum.className = "itemvalue";
+		newForm.className = "itemvalue";
+		newButton.className = "itemvalue";
+
+		newSum.id = linkName.concat("Sum");
+		newForm.id = linkName.concat("Form");
+		newButton.id = linkName.concat("Button");
+		
+		
+		
+		
+		
+		
+
+		newButton.onclick = function(){
+			var num1 = document.getElementById(newSum.id).innerHTML;
+			var num2 = document.getElementById(newForm.id).value;
+			var sum = parseInt(num1, 10) + parseInt(num2, 10);
+
+			document.getElementById(newSum.id).innerHTML = sum;
+
+			var denotationText = document.createTextNode(denotation);
+			newSum.appendChild(denotationText);
+
+			var currentTotal = document.getElementById("totalsum").innerHTML;
+			var tempTotal = parseInt(currentTotal, 10) + parseInt(num2, 10);
+			
+
+			handlers.getSums()
+			handlers.saveSums(tempTotal);
+			document.getElementById("totalsum").innerHTML = tempTotal;
+			
+			
+		}
+
+		newLi.appendChild(newSum);
+		newLi.appendChild(newForm);
+		newLi.appendChild(newButton);
+
+
+		currentList.appendChild(newLi);
+		
+	}
+
+
+
+}
+
+function addMoney(){
+
+}
+
+var handlers = {
+
+	saveSums : function(sumToSave){
+		localStorage.setItem("ptotal", sumToSave);
+	},
+	getSums : function(){
+		var retrievedSum = localStorage.getItem("ptotal");
+		var saved = JSON.parse(retrievedSum);
+		return saved;
+
+	},
+	clearSums : function() {
+		localStorage.clear();
+		return false;
+	}
+};
+
+window.onload = function(){
+	var savedSums = handlers.getSums
+}
+
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Ingen webserver. Standard html/css/js filer.
+
+Jobbet sammen med Kristoffer Sørensen, ettersom dette var tillat.
